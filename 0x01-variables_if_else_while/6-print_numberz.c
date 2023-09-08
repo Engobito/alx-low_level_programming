9f3fe14b388d71711da62a658a51cc5d8c59596c
Engobito <sonymohamed15@gmail.com>
2023-09-08 20:17:40 +0000

    ok

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+	putchar(a);
+	a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
+
