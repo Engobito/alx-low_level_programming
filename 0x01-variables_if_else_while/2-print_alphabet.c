e2b27c790821b2bc2e5ab1fa2b27f1a2f1ce0f79
Engobito <sonymohamed15@gmail.com>
2023-09-08 19:34:56 +0000

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+	int a = 'a', z = 'z';
+
+	while (n <= z)
+	{
+	putchar(n);
+	n = n + z;
+	}
+	putchar('\n');
+	return (0);
+}
+
