514449cab1b4178950e732ea5087f7c3e35181c4
Engobito <sonymohamed15@gmail.com>
2023-09-08 20:44:02 +0000

    ok

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+	putchar(a);
+	if (a == 57)
+	{
+	putchar('$');
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
