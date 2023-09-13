75eaf59221fe321a11f73b82f043bc1b27e4d9db
Engobito <sonymohamed15@gmail.com>
2023-09-13 16:22:25 +0000

    ok

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two integer numbers
+ *
+ * @n: is number 1
+ * @m: is number 2
+ *
+ * Return: Alawys sum (Success)
+*/
+
+int add(int n, int m)
+{
+	int sum;
+
+	sum = n + m;
+	return (sum);
+}
