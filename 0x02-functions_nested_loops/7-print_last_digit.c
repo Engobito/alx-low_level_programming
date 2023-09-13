0b78f924a81a75f103e51071bf55b13830a1402f
Engobito <sonymohamed15@gmail.com>
2023-09-13 15:53:59 +0000

    ok

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - A function that found last digit of number
+ *
+ * @r: The number
+ *
+ * Return: Alawys r (Success)
+*/
+
+int print_last_digit(int r)
+{
+	r = r % 10;
+	return (r);
+}
