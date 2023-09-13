9d9eeb12bad7644e954b4be50774919d829d5fbc
Engobito <sonymohamed15@gmail.com>
2023-09-13 07:07:18 +0000

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Entry point
@@ -15,7 +15,7 @@ int main(void)
 
 	for (i = 0; i < 8; i++)
 	{
-	_putchar(char str[i]);
+	_putchar(str[i]);
 	}
 	_putchar('\n');
 	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,7 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_h
 #include <stdio.h>
 
 int _putchar(char c);
-
 
 #endif
