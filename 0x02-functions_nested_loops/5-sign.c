fd4ca12bf90f32213c8d0d49f415bc494a42ad45
Engobito <sonymohamed15@gmail.com>
2023-09-13 15:18:38 +0000

    ok

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign: A function that found the sign of number
+ *
+ * @n: The number
+ *
+ * Return: 1 is number is positive
+ *         -1 is number is negative
+ *         0 is number is zero
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar('-');
+	return (-1);
+	}
+	else
+	{
+	_putchar('0');
+	return (0);
+	}
+}
