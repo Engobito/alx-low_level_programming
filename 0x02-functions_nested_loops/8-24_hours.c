18fdaf45af91b4294c4d3d2ff7e6eb9bd1b2ce28
Engobito <sonymohamed15@gmail.com>
2023-09-14 02:47:15 +0000

    ok

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minuet of the day
+*/
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
