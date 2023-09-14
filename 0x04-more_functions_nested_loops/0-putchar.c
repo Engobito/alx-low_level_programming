8427f58e10e42cf13d0c3b43ba9b174f16827f74
Engobito <sonymohamed15@gmail.com>
2023-09-14 17:07:17 +0000

    ok

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+	_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
