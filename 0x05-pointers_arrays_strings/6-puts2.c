2ce8a6e6ae4ab33db31265e2a8b596afc9c89b7c
Engobito <sonymohamed15@gmail.com>
2023-09-18 22:25:42 +0000

    ok

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - the function that print 
+ *
+ * @str: the value
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
